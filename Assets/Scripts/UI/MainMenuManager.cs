38bdaed847e96d8762ba49da817f33027546fb6f
okgun <okgun@naver.com>
2024-09-23 05:24:22 +0900

    - TItleScene 메뉴 버튼 작업
      : StartGame 버튼 작업
      : ExitGame 버튼 작업
      : Option 버튼 미작업

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button optionButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private GameObject mainMenuPrefab;
+
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    
+    private void Init()
+    {
+        // pauseMenuPrefab.SetActive(false);
+        startButton.onClick.AddListener(OnStartButtonClicked);
+        optionButton.onClick.AddListener(OnOptionButtonClicked);
+        exitButton.onClick.AddListener(OnExitButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Crush_Prototype");
+    }
+
+    private void OnOptionButtonClicked()
+    {
+
+    }
+
+    private void OnExitButtonClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+
+    private void OnDestroy()
+    {
+        startButton.onClick.RemoveListener(OnStartButtonClicked);
+        optionButton.onClick.RemoveListener(OnOptionButtonClicked);
+        exitButton.onClick.RemoveListener(OnExitButtonClicked);
+    }
+
+
+}
